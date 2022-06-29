c8f439678918dd4b93c040fce46938090fd08472
ElCriSN <cristian.faundezp@utem.cl>
2022-06-29 18:48:18 -0400

    :))!!!
    :)!!!

---
+++ dogyears/app.js
@@ -0,0 +1,12 @@
+const ourAge = 30;
+let earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears = ourAge - 2;
+laterYears *= 4;
+console.log(earlyYears, laterYears); 
+
+ourAgeInDogYears = (earlyYears + laterYears);
+//add earlyYears and laterYears together =)!!! hahjahaa =DDD!!
+
+var ourName = "Generación 33 FSJS =DDDD!!".toLowerCase();
+console.log(`Our name is ${ourName}. We're ${ourAgeInDogYears} years old in dog years`);
