1a1acc20e0a5d5ef3a7c780fe074fba874601c9b
ElCriSN <cristian.faundezp@utem.cl>
2022-06-29 12:34:58 -0400

    :)!!!
    :)!!!!

---
+++ kelvinweather/app.js
@@ -0,0 +1,9 @@
+const kelvin = 0;
+//this is the Temperature in Kelvin Styles  hahahhaa =DDD!!
+const celsius = kelvin - 273;
+//this is the Temperature in Celsius Styles haahhaaa =DDD!!!
+let fahrenheit = celsius * (9 / 5) + 32;
+//this is the Temperature in Fahrenheit Styles haahhaa =DDDD!! =)!!!! :)!!
+fahrenheit = Math.floor(fahrenheit);
+//this is the converter from Celsius to Fahrenheit hahahaaa =DDDD !! =)!!!!
+console.log(`The temperature is ${fahrenheit} degress Fahrenheit.`)
